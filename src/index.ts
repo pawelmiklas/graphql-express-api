8d0124d2db22eefc4efd76df4989e1a0c7929329
Paweł Mikłas <pablomiklas@gmail.com>
2024-05-06 16:29:21 +0200

    build: init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use(cors())
+app.use(express.json())
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
